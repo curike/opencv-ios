f2069beb84faaf586e4e8ce3a54b54466164def8
YutaNomura <nomura@arouse-inc.jp>
2023-10-20 13:22:01 +0900

    output comment

--- create_xcframework.sh
+++ create_xcframework.sh
@@ -15,18 +15,30 @@ OUTPUT_DIR="${SCRIPT_DIR}/frameworks"
 
 build_xcframework="${OPENCV_DIR}/platforms/apple/build_xcframework.py"
 
+echo "Generate OpenCV iOS Framework [${OPENCV_VERSION}]" >&1
+
+# Check version
 cd "${SCRIPT_DIR}"
 
+echo "Check cmake version" >&1
 brew list cmake || brew install cmake
+
+echo "Check pyenv version" >&1
 brew list pyenv || brew install pyenv
 pyenv install ${PYTHON_VERSION} --skip-existing
 eval "$(pyenv init --path)"
 pyenv global ${PYTHON_VERSION}
 
+# Remove tmp dirs
+echo "Remove tmp dirs" >&1
+
 rm -rf "${OPENCV_DIR}"
 rm -rf "${OUTPUT_DIR}/${XCFRAMEWORK}"
 rm -rf "${CONTRIB_DIR}"
 rm -rf "${OUTPUT_DIR}"
+
+# Download
+echo "Download OpenCV ${OPENCV_VERSION}" >&1
 
 curl -L -o opencv.zip "https://github.com/opencv/opencv/archive/refs/tags/${OPENCV_VERSION}.zip"
 unzip -q opencv.zip && rm opencv.zip
@@ -36,8 +48,10 @@ curl -L -o opencv_contrib.zip "https://github.com/opencv/opencv_contrib/archive/
 unzip -q opencv_contrib.zip && rm opencv_contrib.zip
 mv "opencv_contrib-${OPENCV_VERSION}" opencv_contrib
 
-cd "${OPENCV_DIR}"
+# Build
+echo "Build OpenCV ${OPENCV_VERSION}" >&1
 
+cd "${OPENCV_DIR}"
 python3 ${build_xcframework} \
   --iphoneos_archs arm64 \
   --iphonesimulator_archs x86_64,arm64 \
@@ -47,10 +61,19 @@ python3 ${build_xcframework} \
   --framework_name ${FRAMEWORK_NAME} \
   --contrib ../opencv_contrib
 
+echo "Build Success!!" >&1
+
+# Remove tmp dirs
+echo "Clear tmp dirs" >&1
 cd "${SCRIPT_DIR}"
 rm -rf "${OPENCV_DIR}"
 rm -rf "${CONTRIB_DIR}"
 
+# Zip
+echo "Zip framework" >&1
 cd "${OUTPUT_DIR}"
 zip -r "${XCFRAMEWORK}.zip" "${XCFRAMEWORK}"
+
+# Checksum
+echo "checksum" >&1
 swift package compute-checksum "${XCFRAMEWORK}.zip"
